3225e5e56e849827313f62e669d4cdeb36203911
Uri Katz <uri.katz@aquant.io>
2021-08-28 15:00:28 +0300

    host

--- elasticsearch_index/run_index.sh
+++ elasticsearch_index/run_index.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /home/joberant/home/urikatz1/anaconda3/bin/python /home/joberant/home/urikatz1/strategyqa/elasticsearch_index/index_wikipedia.py \
--h localhost /home/joberant/home/urikatz1/dpr/datasets/strategyqa/corpus/corpus-enwiki-20200511-cirrussearch-parasv2.jsonl strategyqa
+-es_h localhost /home/joberant/home/urikatz1/dpr/datasets/strategyqa/corpus/corpus-enwiki-20200511-cirrussearch-parasv2.jsonl strategyqa
